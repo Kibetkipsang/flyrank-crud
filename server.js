02719e2803c8caf78146e1a91ad3fdfeb2a35c4b
Kibetkipsang <denniskipsang2000@gmail.com>
2026-07-28 13:48:26 +0300

    create with validation

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 
 const PORT = 3000;
+app.use(express.json());
+
 
 const tasks = [
     {
@@ -52,6 +54,26 @@ app.get("/tasks/:id", (req, res) => {
 
     res.json(task);
 })
+
+app.post("/tasks", (req, res) => {
+    const { title } = req.body;
+
+    if (!title || title.trim() === "") {
+        return res.status(400).json({
+            error: "Title is required"
+        });
+    }
+
+    const newTask = {
+        id: tasks.length + 1,
+        title: title,
+        done: false
+    };
+
+    tasks.push(newTask);
+
+    res.status(201).json(newTask);
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`);
