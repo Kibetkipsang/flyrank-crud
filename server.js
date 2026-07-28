18aa132f39cd83edf74ef359987d69e4ed6e5eab
Kibetkipsang <denniskipsang2000@gmail.com>
2026-07-28 13:12:37 +0300

    read endpoints with 404

--- server.js
+++ server.js
@@ -3,18 +3,54 @@ const app = express();
 
 const PORT = 3000;
 
+const tasks = [
+    {
+        id: 1,
+        title: "Learn Express",
+        done: false
+    },
+    {
+        id: 2,
+        title: "Build todo API",
+        done: false
+    },
+    {
+        id: 3,
+        title: "Learn CRUD",
+        done: true
+    }
+]
+
 app.get("/", (req, res) => {
     res.json({
         "name": "Task API",
         "version": "1.0",
         "endpoints": ["/tasks"]
-    })
-})
+    });
+});
 
 app.get("/health", (req, res) => {
     res.json({
         "status": "OK"
-    })
+    });
+});
+
+app.get("/tasks", (req, res) => {
+    res.json(tasks);
+})
+
+app.get("/tasks/:id", (req, res) => {
+    const id = Number(req.params.id)
+    const task = tasks.find((task) => {
+        task.id === id
+    });
+    if(!task){
+        return res.status(404).json({
+            "error": `Task ${id} not found`
+        });
+    }
+
+    res.json(task);
 })
 
 app.listen(PORT, () => {
